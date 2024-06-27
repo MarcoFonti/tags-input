dbefcac1152ccaed0306650dfc4a82264f6a3331
Marco Fonti <theemporriumg@gmail.com>
2024-06-27 11:47:37 +0200

    add event vue.js

---
+++ src/components/TagsInput.vue
@@ -0,0 +1,29 @@
+<!-- JS -->
+<script>
+
+export default {
+    name: 'TagsInput',
+    
+    data() {
+        return {
+            tags : ["vue", "react", "angular"],
+            newTag: 'Laravel'
+        }
+    },
+};
+
+</script>
+
+<!-- HTML -->
+<template>
+
+
+<div v-for="(tag, index) in tags">{{ index + ' : ' + tag }}</div>
+
+<!-- 'KEYDOWN.ENTER' = ALL'INVIO , '$EVENT.TARGET.VALUE' = OTTINIAMO IL VALORE DELL'INPUT, 'KEYDOWN.TAB.PREVENT' = ALL'INVIO DEL TAB-->
+<input type="text" :value="newTag" @keydown.enter="tags.push($event.target.value)" @keydown.tab.prevent="tags.push($event.target.value)">
+
+</template>
+
+<!-- CSS -->
+<style scoped></style>
